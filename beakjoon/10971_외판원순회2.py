6bb5912f65539ca87a802c4b13a7978dd59cec76
Woo <kimhyn21@gmail.com>
2023-07-21 18:43:27 +0900

    Update 10971_외판원순회2.py

--- beakjoon/10971_외판원순회2.py
+++ beakjoon/10971_외판원순회2.py
@@ -1,14 +1,23 @@
 import sys
-from collections import deque
+from itertools import permutations
+
 n = int(sys.stdin.readline())
 citys = []
 for _ in range(n):
     cost = list(map(int, sys.stdin.readline().split()))
     citys.append(cost)
 
+per = [i for i in range(n)]
+all_case = list(permutations(per, n))
 
 answer = float("inf")
-que = deque([[i, 0, 0] for i in range(n)])
-while que:
-    c, cost, cnt = que.popleft()
-    
+for case in all_case:
+    temp = 0
+    for i in range(n):
+        temp += citys[case[i-1]][case[i]]
+        if temp > answer or not citys[case[i-1]][case[i]]:
+            temp = 0
+            break
+    if temp < answer and temp:
+        answer = temp
+print(answer)
