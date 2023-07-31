5377fcf6c4ce996c47a5e87f8f298985642a6969
Woo <kimhyn21@gmail.com>
2023-07-31 11:05:16 +0900

    Update 9095_1, 2, 3 더하기.py

--- beakjoon/9095_1, 2, 3 더하기.py
+++ beakjoon/9095_1, 2, 3 더하기.py
@@ -1,12 +1,15 @@
 import sys
 t = int(sys.stdin.readline())
+answer = []
 for _ in range(t):
     n = int(sys.stdin.readline())
-    dp = [0] * (n+1)
-    for i in range(n+1):
+    dp = [0, 1, 2, 4] + [0] * (n-3)
+    for i in range(4, n+1):
         for p in range(1,4):
-            if i - p < 0:
+            if i - p < 1:
                 continue
-            dp[i] += dp[i-p] + 1
+            dp[i] += dp[i-p]
+    answer.append(dp[n])
 
-    print(dp)
+for i in answer:
+    print(i)
