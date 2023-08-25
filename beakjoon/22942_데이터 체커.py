1d203495e156fe82958912ae87a3216392f27e89
Woo <kimhyn21@gmail.com>
2023-08-25 14:05:23 +0900

    Update 22942_데이터 체커.py

--- beakjoon/22942_데이터 체커.py
+++ beakjoon/22942_데이터 체커.py
@@ -3,7 +3,7 @@ import sys
 answer = "YES"
 n = int(sys.stdin.readline())
 circles = []
-for i in range(n):
+for i in range(1, n+1):
     x, r = map(int, sys.stdin.readline().split())
     circles.append((x - r, -i))
     circles.append((x + r, i))
