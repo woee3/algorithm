d91fab7af783c3896d9e05f2a495f5f229a30f0a
woe3 <kimhyn21@gmail.com>
2023-08-28 20:23:00 +0900

    Update 12920_선입 선출 스케줄링.py

--- programers/12920_선입 선출 스케줄링.py
+++ programers/12920_선입 선출 스케줄링.py
@@ -5,26 +5,25 @@ def solution(n, cores):
     answer = 0
     heap = []
     for i in range(len(cores)):
-        heapq.heappush(heap, [cores[i], i+1, cores[i]])
+        heapq.heappush(heap, [cores[i], i + 1, cores[i]])
     n -= len(cores)
 
     while n > 0:
-        temp = [heapq.heappop(heap)]
+        low = heapq.heappop(heap)
+        low[0] += low[2]
+        heapq.heappush(heap, low)
+        temp = [low[1]]
 
-        while heap and temp[0][0] == heap[0][0]:
-            temp.append(heapq.heappop(heap))
-        temp.sort()
-
-        for t, num, c in temp:
-            if n == 0:
-                heapq.heappush(heap, [t, num, c])
-            else:
-                heapq.heappush(heap, [t + c, num, c])
-                n -= 1
-        print(heap)
-    heap = list(heap)
-    heap.sort()
-    return heap[-1][1]
+        while heap and low[0] == heap[0][0]:
+            h = heapq.heappop(heap)
+            h[0] += h[2]
+            heapq.heappush(heap, h)
+            temp.append(h[1])
+        print(temp)
+        if n <= len(temp):
+            temp.sort()
+            return temp[n - 1]
+        n -= len(temp)
 
 
 print(solution(6, [1, 2, 3]))
