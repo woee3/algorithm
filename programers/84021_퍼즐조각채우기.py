da587bb2f65c3b00c6382a904614f07c3934d894
Woo <kimhyn21@gmail.com>
2023-10-13 18:40:13 +0900

    Update 84021_퍼즐조각채우기.py

--- programers/84021_퍼즐조각채우기.py
+++ programers/84021_퍼즐조각채우기.py
@@ -1,7 +1,9 @@
 from collections import deque
 
 def solution(game_board, table):
-    answer = -1
+    answer = 0
+    blank = []
+    cnt = []
     def bfs(y, x, board, find):
         if board[y][x] != find:
             return
@@ -28,12 +30,34 @@ def solution(game_board, table):
                     max_y = max(max_y, y+i)
                     min_x = min(min_x, x+j)
                     max_x = max(max_x, x+j)
-
-        shape = [[0]*(max_x- min_x+1) for _ in range(max_y-min_y+1)]
+        length = max(max_x- min_x+1, max_y-min_y+1)
+        shape = [[0]*(length) for _ in range(length)]
+        if find == 1:
+            cnt.append(len(piece))
         for i, j in piece:
             shape[i-min_y][j-min_x] = 1
         return shape
 
+    def rotate(piece):
+        rpiece = [list(l) for l in zip(*piece[::-1])]
+
+        return rpiece
+
+    for i in range(len(game_board)):
+        for j in range(len(game_board[0])):
+            temp = bfs(i, j, game_board, 0)
+            if temp:
+                blank.append(temp)
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            temp = bfs(i, j, table, 1)
+            if temp:
+                for _ in range(4):
+                    temp = rotate(temp)
+                    if temp in blank:
+                        blank.pop(blank.index(temp))
+                        answer += cnt[-1]
+                        break
     return answer
 
 print(solution([[1, 1, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0], [0, 1, 1, 0, 0, 1], [1, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0], [0, 1, 1, 1, 0, 0]], [[1, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 1, 0], [0, 1, 0, 0, 0, 0]]))
