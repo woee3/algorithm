3c121832c0cfb5fb0f270064df77058f23914378
Woo <kimhyn21@gmail.com>
2023-12-07 19:32:08 +0900

    Create 08_PI.py

---
+++ algorithm_solve_strategy/08_PI.py
@@ -0,0 +1,43 @@
+import sys
+
+c = int(sys.stdin.readline())
+answer = []
+def determine(numbers, i, n):
+    num = numbers[i-n+1:i+1]
+    d = int(num[1]) - int(num[0])
+
+    # case lv 1
+    if len(set(num)) == 1:
+        return 1
+
+
+
+    # case lv 4
+    if len(set(num[0::2])) == 1 and len(set(num[1::2])) == 1:
+        return 4
+
+    # case lv 2, lv 5
+    case = True
+    for l in range(2, n):
+        if int(num[l]) != int(num[0]) + (d * l):
+           case = False
+    if case:
+        if abs(d) == 1:
+            return 2
+        return 5
+
+    return 10
+
+for _ in range(c):
+    numbers = list(sys.stdin.readline().strip())
+    dp = [len(numbers) * 10] * len(numbers)
+    dp[2] = determine(numbers, 2, 3)
+    dp[3] = determine(numbers, 3, 4)
+    dp[4] = determine(numbers, 4, 5)
+    for i in range(5, len(numbers)):
+        for k in range(3, 6):
+            dp[i] = min(dp[i], determine(numbers, i, k) + dp[i-k])
+    answer.append(dp[-1])
+for a in answer:
+    print(a)
+
